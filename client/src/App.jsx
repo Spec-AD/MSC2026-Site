b48c02fadf69c5361a2275dea9ceb7267caf3882
Amamiya Reo <wolonguis@gmail.com>
2026-02-27 15:30:58 +0800

    Add files via upload

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastProvider } from './context/ToastContext'; // 🔥 新增：全局 Toast 提示引擎
 import Layout from './components/Layout';
 import Admin from './pages/Admin';
 import Inbox from './pages/Inbox';
@@ -12,7 +13,7 @@ import Rules from './pages/Rules';
 import Qualifiers from './pages/Qualifiers';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
-import Songs from './pages/Songs'; // 🔥 新增：引入曲目图鉴页面
+import Songs from './pages/Songs'; // 🔥 曲目图鉴页面
 import Feedback from './pages/Feedback';
 import Tournaments from './pages/Tournaments';
 import TournamentInfo from './pages/TournamentInfo';
@@ -22,40 +23,43 @@ import WikiArticle from './pages/WikiArticle';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* 外层路由：使用 Layout 布局 */}
-        <Route path="/" element={<Layout />}>
-          
-          {/* 默认首页 */}
-          <Route index element={<Home />} />
-          
-          {/* 各个功能页 */}
-          <Route path="register" element={<Register />} />
-          <Route path="intro" element={<Intro />} />
-          <Route path="rules" element={<Rules />} />
-          <Route path="qualifiers" element={<Qualifiers />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/profile/:username" element={<Profile />} />
-          <Route path="/admin" element={<Admin />} />
-	  <Route path="/feedback" element={<Feedback />} />
-          <Route path="/inbox" element={<Inbox />} />
-          <Route path="/tournaments" element={<Tournaments />} />
-	  <Route path="/tournament-info" element={<TournamentInfo />} />
-	  <Route path="/wiki" element={<WikiIndex />} />
-          <Route path="/wiki/:slug" element={<WikiArticle />} />
-	  <Route path="/leaderboard" element={<Leaderboard />} />
-          
-          {/* 🔥 新增：曲目图鉴页路由 */}
-          <Route path="songs" element={<Songs />} />
-          
-          {/* 登录页 */}
-          <Route path="login" element={<Login />} />
+      {/* 🔥 用 ToastProvider 包裹整个路由，使漂亮的弹窗在全站任意页面均可调用 */}
+      <ToastProvider>
+        <Routes>
+          {/* 外层路由：使用 Layout 布局 */}
+          <Route path="/" element={<Layout />}>
+            
+            {/* 默认首页 */}
+            <Route index element={<Home />} />
+            
+            {/* 各个功能页 */}
+            <Route path="register" element={<Register />} />
+            <Route path="intro" element={<Intro />} />
+            <Route path="rules" element={<Rules />} />
+            <Route path="qualifiers" element={<Qualifiers />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/profile/:username" element={<Profile />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/feedback" element={<Feedback />} />
+            <Route path="/inbox" element={<Inbox />} />
+            <Route path="/tournaments" element={<Tournaments />} />
+            <Route path="/tournament-info" element={<TournamentInfo />} />
+            <Route path="/wiki" element={<WikiIndex />} />
+            <Route path="/wiki/:slug" element={<WikiArticle />} />
+            <Route path="/leaderboard" element={<Leaderboard />} />
+            
+            {/* 曲目图鉴页路由 */}
+            <Route path="songs" element={<Songs />} />
+            
+            {/* 登录页 */}
+            <Route path="login" element={<Login />} />
 
-          {/* 404 处理：随便输什么都跳回主页 */}
-          <Route path="*" element={<Navigate to="/" replace />} />
-          
-        </Route>
-      </Routes>
+            {/* 404 处理：随便输什么都跳回主页 */}
+            <Route path="*" element={<Navigate to="/" replace />} />
+            
+          </Route>
+        </Routes>
+      </ToastProvider>
     </BrowserRouter>
   );
 }
